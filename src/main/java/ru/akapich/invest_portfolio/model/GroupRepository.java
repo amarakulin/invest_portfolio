d9980ec9b5173682350b144492e6a143ce0e305c
pichkasik <sasha.marakulin@mail.ru>
2021-05-31 23:20:56 +0300

    Delete tests files

--- src/main/java/ru/akapich/invest_portfolio/model/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.akapich.invest_portfolio.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-	Group findByName(String name);
-}
