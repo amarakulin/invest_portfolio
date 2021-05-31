37642dc34a0d9532950d5500e8ec0bede75c036f
pichkasik <sasha.marakulin@mail.ru>
2021-05-31 20:42:03 +0300

    создание базового spring boot

---
+++ src/main/java/ru/akapich/invest_portfolio/InvestPortfolioApplication.java
@@ -0,0 +1,13 @@
+package ru.akapich.invest_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akapich/invest_portfolio/InvestPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akapich.invest_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
