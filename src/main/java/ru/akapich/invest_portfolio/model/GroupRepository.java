d9edf5a073a8b9842652cb6f46cea88aebe7f78c
pichkasik <sasha.marakulin@mail.ru>
2021-05-31 21:29:49 +0300

    Display groups and events

---
+++ src/main/java/ru/akapich/invest_portfolio/model/GroupRepository.java
@@ -0,0 +1,9 @@
+package ru.akapich.invest_portfolio.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+	Group findByName(String name);
+}
