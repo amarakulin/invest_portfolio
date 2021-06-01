f7bcb3c0a7cc90488e112cc4181b5c6c763115cb
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-01 16:29:14 +0300

    add basic Title Component

---
+++ src/front/src/components/basicComponents/Title.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+	text-align: left;
+	font-weight: bold;
+	font-size: 30px;
+	line-height: 36px;
+	color: #000000;
+`;
+
+
+export default Title;
