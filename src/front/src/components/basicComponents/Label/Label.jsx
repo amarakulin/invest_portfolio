1984d3f29022b7c7de14fa768dcad88653533201
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-01 17:57:09 +0300

    add basic Label Component

---
+++ src/front/src/components/basicComponents/Label/Label.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Label = styled.label`
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 19px;
+	color: #696F79;
+	margin-bottom: 8px;
+`;
+
+
+export default Label;
