eba7ae4cfd4a90d6585e51d7d020d973dede9cee
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-01 15:50:47 +0300

    add store provider

--- src/front/src/index.js
+++ src/front/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './redux/redux';
+import {Provider} from 'react-redux';
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider value={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
