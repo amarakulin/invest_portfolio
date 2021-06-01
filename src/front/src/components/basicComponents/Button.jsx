eddbc37ae1faf438a7fcf07ce5c74a526746feed
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-01 16:15:13 +0300

    add basic Button Component

---
+++ src/front/src/components/basicComponents/Button.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+	padding: 21px 118px;
+	background-color: #89D7AB;
+	border: none;
+	border-radius: 5px;
+	color: #fff;
+	transition: background-color 0.2s ease;
+	&:hover {
+		background-color: #74B791;
+	}
+`;
+
+
+export default Button;
