d9bb53ef12267727fc75c114c426205ea3295af6
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-01 17:44:23 +0300

    add basic Subtitle Component

---
+++ src/front/src/components/basicComponents/Subtitle.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Subtitle = styled.h2`
+	text-align: left;
+	font-weight: normal;
+	font-size: 18px;
+	line-height: 28px;
+	color: #8692A6;
+	margin: 0;
+`;
+
+
+export default Subtitle;
