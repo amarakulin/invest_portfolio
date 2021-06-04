a2bb440ca8c8d7be092400285c2878b7452ac5a1
pichkasik <sasha.marakulin@mail.ru>
2021-06-04 15:48:07 +0300

    Rename ResponseUserDTO

--- src/main/java/ru/akapich/invest_portfolio/DTO/ResponseUserDTO.java
+++ src/main/java/ru/akapich/invest_portfolio/DTO/ResponseUserDTO.java
@@ -5,13 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Id;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ResponceUserDTO {
+public class ResponseUserDTO {
 
 	private String login;
 	private String email;
