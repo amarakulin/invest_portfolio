cfb6d3b274f7f7a2ca0b49896e423d9ddfd001c2
pichkasik <sasha.marakulin@mail.ru>
2021-06-05 18:44:38 +0300

    Add UserService

---
+++ src/main/java/ru/akapich/invest_portfolio/service/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package ru.akapich.invest_portfolio.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.akapich.invest_portfolio.model.User;
+import ru.akapich.invest_portfolio.repository.UserRepository;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+		User user = userRepository.getUserByLogin(s);
+
+		if (user == null){
+			throw new UsernameNotFoundException("Неправильный Логин/Пароль");
+		}
+		else{
+			Collection<SimpleGrantedAuthority> roles = new HashSet<>();
+			roles.add(new SimpleGrantedAuthority(user.getRole()));
+			return new org.springframework.security.core.userdetails.User(
+					user.getLogin(),
+					user.getPassword(),
+					user.isEnable(),
+					true,
+					true,
+					true,
+					roles);
+		}
+	}
+}
