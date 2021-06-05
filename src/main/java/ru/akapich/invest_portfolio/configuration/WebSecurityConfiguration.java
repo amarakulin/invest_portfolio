60d4b16775c04252ed2aa6f53bcb575801d30734
pichkasik <sasha.marakulin@mail.ru>
2021-06-05 19:30:19 +0300

    add annotation in Web Security config

--- src/main/java/ru/akapich/invest_portfolio/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/akapich/invest_portfolio/configuration/WebSecurityConfiguration.java
@@ -2,14 +2,18 @@ package ru.akapich.invest_portfolio.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.akapich.invest_portfolio.service.UserDetailsServiceImpl;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -37,7 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/login").authenticated()
+				.antMatchers("/user").authenticated()
 				.antMatchers("/**").permitAll()
 				.and()
 				.formLogin().permitAll().defaultSuccessUrl("/home")
