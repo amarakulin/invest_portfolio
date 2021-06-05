4eb14af8a018cc1267e3f276c0c7a4f807288bf7
pichkasik <sasha.marakulin@mail.ru>
2021-06-05 19:08:36 +0300

    Add test

--- src/main/java/ru/akapich/invest_portfolio/controller/UserController.java
+++ src/main/java/ru/akapich/invest_portfolio/controller/UserController.java
@@ -1,10 +1,16 @@
 package ru.akapich.invest_portfolio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "success";
+	}
 
 	@PostMapping("/login")
 	public String login(){
