85164ac8b23367a6cce486afd99a6acc4b254e98
pichkasik <sasha.marakulin@mail.ru>
2021-06-06 19:32:12 +0300

    Fix getter setter problem

--- src/main/java/ru/akapich/invest_portfolio/model/User.java
+++ src/main/java/ru/akapich/invest_portfolio/model/User.java
@@ -40,53 +40,53 @@ public class User {
 	@Column
 	private boolean enable;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public boolean isEnable() {
-		return enable;
-	}
-
-	public void setEnable(boolean enable) {
-		this.enable = enable;
-	}
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getLogin() {
+//		return login;
+//	}
+//
+//	public void setLogin(String login) {
+//		this.login = login;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public String getRole() {
+//		return role;
+//	}
+//
+//	public void setRole(String role) {
+//		this.role = role;
+//	}
+//
+//	public boolean isEnable() {
+//		return enable;
+//	}
+//
+//	public void setEnable(boolean enable) {
+//		this.enable = enable;
+//	}
 //TODO Add timestamp create
 	//TODO Add access rules
 }
