4e771bf5bc1e7ce1eda0064a21dba42f56edb98b
pichkasik <sasha.marakulin@mail.ru>
2021-06-06 20:46:10 +0300

    Add bilder to User

--- src/main/java/ru/akapich/invest_portfolio/model/User.java
+++ src/main/java/ru/akapich/invest_portfolio/model/User.java
@@ -1,9 +1,6 @@
 package ru.akapich.invest_portfolio.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -18,14 +15,12 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class User {
 
-
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	//TODO do some with ID
 	@Id
-	@NonNull
 	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column
@@ -39,55 +34,7 @@ public class User {
 
 	@Column
 	private boolean enable;
-
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String getLogin() {
-//		return login;
-//	}
-//
-//	public void setLogin(String login) {
-//		this.login = login;
-//	}
-//
-//	public String getEmail() {
-//		return email;
-//	}
-//
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//
-//	public String getPassword() {
-//		return password;
-//	}
-//
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-//
-//	public String getRole() {
-//		return role;
-//	}
-//
-//	public void setRole(String role) {
-//		this.role = role;
-//	}
-//
-//	public boolean isEnable() {
-//		return enable;
-//	}
-//
-//	public void setEnable(boolean enable) {
-//		this.enable = enable;
-//	}
-//TODO Add timestamp create
+	//TODO Add timestamp create
 	//TODO Add access rules
 }
 
