507e65707b106523f91ae0522428f5642b9c4e6f
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-06 20:51:10 +0300

    add initial AuthAPI

---
+++ src/front/src/api/api.js
@@ -0,0 +1,15 @@
+import * as axios from 'axios';
+
+const instanse = axios.create({
+	baseURL: 'http://localhost:8080/'
+});
+
+export const AuthAPI = {
+	login(login, password, rememberMe = false) {
+		return instanse.post('/login', {
+			login,
+			password,
+			rememberMe
+		})
+	}
+}
