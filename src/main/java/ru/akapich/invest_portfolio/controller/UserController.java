108254cde08f4a1ebb7b4c3653471bba97e757ab
pichkasik <sasha.marakulin@mail.ru>
2021-06-06 12:47:18 +0300

    Add test in UserController

--- src/main/java/ru/akapich/invest_portfolio/controller/UserController.java
+++ src/main/java/ru/akapich/invest_portfolio/controller/UserController.java
@@ -2,18 +2,24 @@ package ru.akapich.invest_portfolio.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.akapich.invest_portfolio.model.User;
 
 @RestController
 public class UserController {
 
 	@GetMapping("/home")
 	public String home() {
-		return "success";
+
+		System.out.println("/home! ");
+		return "success user IN ";
 	}
 
-	@PostMapping("/login")
-	public String login(){
-		return "Login";
+	@PostMapping("/post")
+	public String post(@RequestBody User user) {
+
+		System.out.println("/post! " + user.getLogin());
+		return "success user IN " + user.getEmail();
 	}
 }
