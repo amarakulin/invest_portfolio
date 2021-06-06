1e0890b47d99d8969162d948ec85136bde9c6449
pichkasik <sasha.marakulin@mail.ru>
2021-06-06 12:11:40 +0300

    Make field userRepository private

--- src/main/java/ru/akapich/invest_portfolio/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/akapich/invest_portfolio/service/UserDetailsServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.HashSet;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
