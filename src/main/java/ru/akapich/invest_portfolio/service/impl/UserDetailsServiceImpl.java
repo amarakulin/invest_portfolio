8d7a4a6ba9a743e4ecc4a37b1e0d1937f27b0da6
pichkasik <sasha.marakulin@mail.ru>
2021-06-06 17:09:29 +0300

    Move to extra packege

--- src/main/java/ru/akapich/invest_portfolio/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/ru/akapich/invest_portfolio/service/impl/UserDetailsServiceImpl.java
@@ -1,10 +1,13 @@
 package ru.akapich.invest_portfolio.service;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.akapich.invest_portfolio.model.User;
 import ru.akapich.invest_portfolio.repository.UserRepository;
@@ -13,14 +16,18 @@ import java.util.Collection;
 import java.util.HashSet;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService, UserService {
 
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
 	@Override
-	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-		User user = userRepository.getUserByLogin(s);
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.getUserByLogin(username);
 
 		if (user == null){
 			throw new UsernameNotFoundException("Неправильный Логин/Пароль");
@@ -37,5 +44,15 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 					true,
 					roles);
 		}
+	}
+
+	@Override
+	public void save(String s){
+		//TODO check if exist user
+		System.out.println(s.length());
+//		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+//		user.setRole("ROLE_USER");
+//		userRepository.save(user);
+		System.out.println("Saved user");
 	}
 }
