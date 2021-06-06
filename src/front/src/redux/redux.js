5f1680b5f71bbaab4874915de065bf03a044d059
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-06 20:29:49 +0300

    add applyMiddleware to store

--- src/front/src/redux/redux.js
+++ src/front/src/redux/redux.js
@@ -1,11 +1,12 @@
-import { createStore , combineReducers } from 'redux';
+import { createStore , combineReducers, applyMiddleware } from 'redux';
 import authReduser from './authReduser.js';
+import thunkMiddleware from 'redux-thunk';
 
 const reduser = combineReducers({
 	auth: authReduser,
 });
 
-const store = createStore(reduser);
+const store = createStore(reduser, applyMiddleware(thunkMiddleware));
 
 export default store;
 
