78d413f967c118ae3d01d1af7d09d9a4be306d25
pichkasik <sasha.marakulin@mail.ru>
2021-06-06 12:34:52 +0300

    Change strength on 10

--- src/main/java/ru/akapich/invest_portfolio/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/akapich/invest_portfolio/configuration/WebSecurityConfiguration.java
@@ -21,7 +21,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public  PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder(12);
+		return new BCryptPasswordEncoder(10);
 	}
 
 	@Bean
