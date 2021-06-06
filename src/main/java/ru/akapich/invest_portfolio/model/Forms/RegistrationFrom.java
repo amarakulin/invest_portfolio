aeffd0a43d9634653be694f861208eb49e9003dc
pichkasik <sasha.marakulin@mail.ru>
2021-06-06 22:54:39 +0300

    Add Valid -> Registration Form

--- src/main/java/ru/akapich/invest_portfolio/model/Forms/RegistrationFrom.java
+++ src/main/java/ru/akapich/invest_portfolio/model/Forms/RegistrationFrom.java
@@ -3,14 +3,31 @@ package ru.akapich.invest_portfolio.model.Forms;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.akapich.invest_portfolio.validator.custom_interfaces.ExistingLogin;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegistrationFrom {
 
+	@NotBlank(message = "valid.empty.login")
+	@Size(min = 5, max = 32, message = "valid.size.login")
+	@ExistingLogin
 	private String login;
+
+	@NotBlank(message = "valid.empty.email")
+	@Email(message = "valid.email")
 	private String email;
+
+	@NotBlank(message = "valid.empty.password")
+	@Size(min = 5, max = 32, message = "valid.size.password")
 	private String password;
+
+	@NotBlank(message = "valid.empty.password")
+	@Size(min = 5, max = 32, message = "valid.size.password")
 	private String confirmPassword;
 }
