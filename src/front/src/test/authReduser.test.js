7af052d95736f3deabf7c49593bb8b20ded54743
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-07 15:48:29 +0300

    add fake test for login func

--- src/front/src/test/authReduser.test.js
+++ src/front/src/test/authReduser.test.js
@@ -23,7 +23,7 @@ it('Fake login should return error', () => {
 
 	const processingLoginResponse = (res) => {
 		if (res.resultCode === 0) {
-			dispatch(setAuthUserData(res.userID, res.email, res.login, res.isAuth));
+			// dispatch(setAuthUserData(res.userID, res.email, res.login, res.isAuth));
 		} else {
 			return res.errorMessage;
 		}
@@ -33,5 +33,53 @@ it('Fake login should return error', () => {
 		.then(processingLoginResponse)
 		.then(res => {
 			expect(res).toBe('error')
+		})
+})
+
+it('Fake rember me should set value', () => {
+	const fakeLogin = new Promise(resolve => {
+		resolve({resultCode: 0, errorMessage: 'error',
+			login: 'akasha', email: "akasha@mail.ru", userID: 1, isAuth: true})
+	})
+
+	const processingLoginResponse = (res) => {
+		if (res.resultCode === 0) {
+			// dispatch(setAuthUserData(res.userID, res.email, res.login, res.isAuth));
+			if (res.isAuth)
+				return 'setItem'
+		} else {
+			return res.errorMessage;
+		}
+	}
+
+	fakeLogin
+		.then(processingLoginResponse)
+		.then(res => {
+			expect(res).toBe('setItem')
+		})
+})
+
+it("Fake rember me shouldn't set value", () => {
+	const fakeLogin = new Promise(resolve => {
+		resolve({resultCode: 0, errorMessage: 'error',
+			login: 'akasha', email: "akasha@mail.ru", userID: 1, isAuth: false})
+	})
+
+	const processingLoginResponse = (res) => {
+		if (res.resultCode === 0) {
+			// dispatch(setAuthUserData(res.userID, res.email, res.login, res.isAuth));
+			if (res.isAuth)
+				return 'setItem'
+			else if (!res.isAuth)
+				return 'unsetItem'
+		} else {
+			return res.errorMessage;
+		}
+	}
+
+	fakeLogin
+		.then(processingLoginResponse)
+		.then(res => {
+			expect(res).toBe('unsetItem')
 		})
 })
