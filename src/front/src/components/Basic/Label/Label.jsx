b39c7f3bab741d97b9eaa86d519431b8055b7044
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-07 20:07:48 +0300

    add form field errors

--- src/front/src/components/Basic/Label/Label.jsx
+++ src/front/src/components/Basic/Label/Label.jsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 const Label = styled.label`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 	font-weight: 500;
 	font-size: 16px;
 	line-height: 19px;
 	color: #696F79;
 	margin-bottom: 8px;
+	&::after {
+		content: '${props => props.error}';
+		display: block;
+		color: tomato;
+	}
 `;
 
 
--- src/front/src/utils/validators.js
+++ src/front/src/utils/validators.js
@@ -2,10 +2,10 @@ export const composeValidators = (...validators) => value =>
 	validators.reduce((error, validator) => error || validator(value), undefined)
 
 export const requiredField = (value) => {
-	return value ? undefined : "Required";
+	return value ? undefined : "Поле является обязтельным";
 }
 
 export const emailValidator = (value) => {
 	const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return reg.test(value) ? undefined : 'invalid email pattern';
+	return reg.test(value) ? undefined : 'Неверный формат e-mail';
 }
