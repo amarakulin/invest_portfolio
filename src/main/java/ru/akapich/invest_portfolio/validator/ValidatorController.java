74bf77cf0d19f5a71590135f4b9b32f956a80f3c
pichkasik <sasha.marakulin@mail.ru>
2021-06-07 14:48:14 +0300

    Fix error if several validaet Annotaion worked

--- src/main/java/ru/akapich/invest_portfolio/validator/ValidatorController.java
+++ src/main/java/ru/akapich/invest_portfolio/validator/ValidatorController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -21,8 +22,17 @@ public class ValidatorController implements ErrorController {
 		Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(
 				fieldError -> fieldError.getField() + "Error",
 				FieldError::getDefaultMessage
-		);
-
-		return bindingResult.getFieldErrors().stream().collect(collector);
+				);
+		Map<String, String> mapError;
+		try{
+			mapError = bindingResult.getFieldErrors().stream().collect(collector);
+		}
+		catch (IllegalStateException e){
+			//If more the one annotation is worked
+			//TODO Ask Georgi
+			mapError = bindingResult.getFieldErrors().stream().limit(1).collect(collector);
+		}
+		return mapError;
 	}
+
 }
