9dbe3a890f630c11c4897bd2b46fa6bfe370fcec
pichkasik <sasha.marakulin@mail.ru>
2021-06-08 20:29:33 +0300

    Add terrable Cord

--- src/main/java/ru/akapich/invest_portfolio/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/akapich/invest_portfolio/configuration/WebSecurityConfiguration.java
@@ -10,6 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Security Configuration.
@@ -44,16 +51,36 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.authenticationProvider(authenticationProvider());
 	}
 
+		/* Let send requests for server */
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+				.cors(withDefaults())
 				.csrf().disable()
 				.authorizeRequests()
 				.anyRequest().authenticated()
 				.and()
-				.formLogin().loginPage("/api/login").permitAll().defaultSuccessUrl("/home")
+				.formLogin().loginProcessingUrl("/api/login").permitAll().defaultSuccessUrl("/home")//
 				.and()
 				.logout().invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll();
+//				.csrf().disable()
+//				.authorizeRequests()
+//				.anyRequest().authenticated()
+//				.and()
+//				.formLogin().loginPage("/api/login").permitAll().defaultSuccessUrl("/home")
+//				.and()
+//				.logout().invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll();
+
 //		http
 //				.csrf().disable()
 //				.authorizeRequests()
