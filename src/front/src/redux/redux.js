8faac90f46b6b6422877a391150c8e5fee976451
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-08 20:12:32 +0300

    add modal reducer to store

--- src/front/src/redux/redux.js
+++ src/front/src/redux/redux.js
@@ -1,9 +1,11 @@
 import { createStore , combineReducers, applyMiddleware } from 'redux';
 import authReduser from './authReduser.js';
+import modalReduser from './modalReduser.js';
 import thunkMiddleware from 'redux-thunk';
 
 const reduser = combineReducers({
 	auth: authReduser,
+	modal: modalReduser,
 });
 
 const store = createStore(reduser, applyMiddleware(thunkMiddleware));
