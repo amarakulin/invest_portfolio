e4973928f238f32ca33eb2f1e2a7fd4ab6d02e89
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-08 15:38:37 +0300

    rewrite proxy

--- src/front/src/setupProxy.js
+++ src/front/src/setupProxy.js
@@ -1,7 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/', 
-        { target: 'http://localhost:8081/' }
-    ));
+    app.use(
+        '/api',
+        createProxyMiddleware({
+          target: 'http://localhost:8081'
+        })
+      );
 }
