9ae35f4fba9194b76592edc3ef0b47dc3e3b099e
pichkasik <sasha.marakulin@mail.ru>
2021-06-13 22:38:06 +0300

    Test controller

--- src/main/java/ru/akapich/invest_portfolio/repository/portfolio/asset_data/FinancialAssetRepository.java
+++ src/main/java/ru/akapich/invest_portfolio/repository/portfolio/asset_data/FinancialAssetRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.akapich.invest_portfolio.model.portfolio.asset_data.FinancialAssetInUse;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
 /**
  * JavaBean object that interaction with Database.
  *
@@ -12,4 +16,8 @@ import ru.akapich.invest_portfolio.model.portfolio.asset_data.FinancialAssetInUs
 
 @Repository
 public interface FinancialAssetRepository extends JpaRepository<FinancialAssetInUse, Integer> {
+
+//	void insertAllAssets(List<String> listAssets);
+
+
 }
