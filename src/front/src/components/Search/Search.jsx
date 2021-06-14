5d56801983b377e12d7bb8be76b0c599d9dfdf93
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-14 16:31:02 +0300

    add SearchResult component to Search component

--- src/front/src/components/Search/Search.jsx
+++ src/front/src/components/Search/Search.jsx
@@ -1,18 +1,41 @@
 // onChange -> GET request with ?text=${input} to api
 import Input from '../Basic/Input/Input';
+import SearchResult from './SearchResult'
 
-const Search = ({input, meta, ...props}) => {
+const res = [
+	{
+		ticker: 'AXC',
+		company: 'APPLE',
+		type: 'акция'
+	},
+	{
+		ticker: 'ZKJ',
+		company: 'MICROSOFT',
+		type: 'акция'
+	},
+	{
+		ticker: 'ANC',
+		company: 'ahsjdbajskdnasjahsjdbajskdnasjahsjdbajskdnasjahsjdbajskdnasjahsjdbajskdnasj',
+		type: 'акция'
+	}
+]
+
+const Search = ({ input, meta, ...props }) => {
 	return (
-		<Input
-			{...input}
-			type='text'
-			placeholder='Поиск'
-			width='70%'
-			autoComplete='off'
-			onChange={(e) => {
-				input.onChange(e);
-			}}
-		/>
+		<>
+			<Input
+				{...input}
+				type='text'
+				placeholder='Поиск'
+				width='70%'
+				autoComplete='off'
+				onChange={e => {
+					input.onChange((e.currentTarget.value ).toUpperCase());
+
+				}}
+			/>
+			<SearchResult data={res}/>
+		</>
 	)
 }
 
