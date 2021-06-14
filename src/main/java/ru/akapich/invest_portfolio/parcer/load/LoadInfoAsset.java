70a7de1db4e233b445f3cc57ce01f4b4a25e5124
pichkasik <sasha.marakulin@mail.ru>
2021-06-14 17:38:45 +0300

    Load data from site

---
+++ src/main/java/ru/akapich/invest_portfolio/parcer/load/LoadInfoAsset.java
@@ -0,0 +1,30 @@
+package ru.akapich.invest_portfolio.parcer.load;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.akapich.invest_portfolio.parcer.america.ParseAmericanStock;
+import ru.akapich.invest_portfolio.service.portfolio.asset_data.AllFinancialAssetImpl;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class that load and insert data assets in database
+ *
+ * @author Aleksandr Marakulin
+ **/
+
+public class LoadInfoAsset {
+
+
+	@Autowired
+	private ParseAmericanStock parseAmericanStock;
+
+	@Autowired
+	private AllFinancialAssetImpl allFinancialAsset;
+
+	public void loadInfoAssetsByAmericanExchange(String nameExchange) throws IOException {
+		List<Map<String, String>> listAssets = parseAmericanStock.getAllStocksByAmericanExchange(nameExchange);
+		allFinancialAsset.insertAllAssets(listAssets);
+	}
+}
