b74f9184f71b5a861b642045acde61fecef35546
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-15 19:09:57 +0300

    add initial click to AddNewAsset

--- src/front/src/components/NewAsset/AddInput.jsx
+++ src/front/src/components/NewAsset/AddInput.jsx
@@ -1,10 +1,17 @@
+import { useRef, useEffect } from 'react'
 import { addNewAsset } from '../../redux/newAssetsReduser';
 import { AddInputStyled } from '../Basic/Button/Button'
 import { connect } from 'react-redux';
 
 const AddInput = (props) => {
+	const buttonRef = useRef(null);
+
+	useEffect(() => {
+		buttonRef.current.click();
+	}, [])
 	return (
-		<AddInputStyled 
+		<AddInputStyled
+			ref={buttonRef}
 			onClick={(e) => {
 				e.preventDefault();
 				props.addNewAsset(props.elem)
