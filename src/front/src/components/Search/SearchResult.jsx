e4776457bc7878710910d6d1b519774a32b9e282
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-17 22:46:34 +0300

    move render search result list to separated function

--- src/front/src/components/Search/SearchResult.jsx
+++ src/front/src/components/Search/SearchResult.jsx
@@ -12,7 +12,7 @@ const SearchResultList = styled.ul`
 	width: 70%;
 	border-radius: 6px;
 	min-height: 50px;
-	${props => props.isFetching && 
+	${props => props.isFetching &&
 		`
 			display: flex; 
 			flex-direction: column;
@@ -61,32 +61,38 @@ const SearchResultType = styled.span`
 	color: #8692A6;
 `
 
+const renderSearchResultList = (props) => {
+	if (props.isFetching)
+		return <Preloader color='black' />
+	else {
+		if (props.searchData.length) {
+			return props.searchData.map((el, i) => {
+				return (
+					<SearchResultListItem
+						onMouseDown={() => {
+							props.nessesary.forEach(el => {
+								props.setInputData(el, props.searchData[i][el])
+							})
+							props.setShowSearch(false);
+						}}
+						key={el.ticker + el.type}
+					>
+						<SearchResultTicker>{el.ticker.toUpperCase()}</SearchResultTicker>
+						<SearchResultCompanyName>{el.name.toUpperCase()}</SearchResultCompanyName>
+						<SearchResultType>{el.type}</SearchResultType>
+					</SearchResultListItem>
+				)
+			})
+		}
+		else
+			return <SearchResultListItem>{'Совпадений не найдено'}</SearchResultListItem>
+	}
+}
+
 const SearchResult = (props) => {
 	return (
 		<SearchResultList isFetching={props.isFetching}>
-			{
-				props.isFetching
-				? <Preloader color='black'/>
-				: props.searchData.length
-					? props.searchData.map((el, i) => {
-						return (
-							<SearchResultListItem 
-								onMouseDown={() => {
-									props.nessesary.forEach(el => {
-										props.setInputData(el, props.searchData[i][el])
-									})
-									props.setShowSearch(false);
-								}}
-								key={el.ticker + el.type}
-							>
-								<SearchResultTicker>{el.ticker.toUpperCase()}</SearchResultTicker>
-								<SearchResultCompanyName>{el.name.toUpperCase()}</SearchResultCompanyName>
-								<SearchResultType>{el.type}</SearchResultType>
-							</SearchResultListItem>
-						)
-					})
-					: <SearchResultListItem>{'Совпадений не найдено'}</SearchResultListItem>
-			}
+			{renderSearchResultList(props)}
 		</SearchResultList>
 	)
 }
