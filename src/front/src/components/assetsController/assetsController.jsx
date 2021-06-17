8f64c6e727591f91bd9a66896ed564d6b081b0c5
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-17 22:35:45 +0300

    add authredirec to assetsController component

--- src/front/src/components/assetsController/assetsController.jsx
+++ src/front/src/components/assetsController/assetsController.jsx
@@ -2,6 +2,7 @@ import Header from '../Header/Header';
 import TopAssetsControllerContent from './TopAssetsControllerContent/TopAssetsControllerContent'
 import AssetsControllerTable from './AssetsControllerTable/AssetsControllerTable'
 import { Container } from '../Basic/Wrapper/Wrapper';
+import { withAuthRedirectToSignUp } from '../../HOC/withRedirect'
 
 
 const assetsController = (props) => {
@@ -16,4 +17,4 @@ const assetsController = (props) => {
 	)
 }
 
-export default assetsController;
+export default withAuthRedirectToSignUp(assetsController);
