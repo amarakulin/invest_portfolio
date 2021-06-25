d6228764418d3b3816da3c95a2b19fe75fbe5855
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-25 14:42:42 +0300

    fix authReduser test

--- src/front/src/test/authReduser.test.js
+++ src/front/src/test/authReduser.test.js
@@ -4,36 +4,32 @@ it('Добавление значений в setAuthUserData', () => {
 	// Исходные данные
 	const initialState = {
 		name: null,
-		email: null,
-		userID: null,
 		isAuth: localStorage.getItem("isAuth") || false,
 	}
 
-	const action = setAuthUserData(1, 'akasha@mail.ru', 'akasha', true);
+	const action = setAuthUserData('akasha', true);
 
 	// Вызываем reduser
 	const newState = authReduser(initialState, action);
 
 	// Проверка
-	expect(newState).toEqual({name: 'akasha', email: "akasha@mail.ru", userID: 1, isAuth: true});
+	expect(newState).toEqual({name: 'akasha', isAuth: true});
 })
 
 it('Обнуление значений в setAuthUserData', () => {
 	// Исходные данные
 	const initialState = {
 		name: 'akasha',
-		email: "akasha@mail.ru",
-		userID: 1,
 		isAuth: true,
 	}
 
-	const action = setAuthUserData(null, null, null, false);
+	const action = setAuthUserData(null, false);
 
 	// Вызываем reduser
 	const newState = authReduser(initialState, action);
 
 	// Проверка
-	expect(newState).toEqual({name: null, email: null ,userID: null ,isAuth: false});
+	expect(newState).toEqual({name: null, isAuth: false});
 })
 
 it('Fake login should return error', () => {
@@ -51,53 +47,5 @@ it('Fake login should return error', () => {
 		.then(processingLoginResponse)
 		.then(res => {
 			expect(res).toBe('error')
-		})
-})
-
-it('Fake rember me should set value', () => {
-	const fakeLogin = new Promise(resolve => {
-		resolve({resultCode: 0, errorMessage: 'error',
-			login: 'akasha', email: "akasha@mail.ru", userID: 1, isAuth: true})
-	})
-
-	const processingLoginResponse = (res) => {
-		if (res.resultCode === 0) {
-			// dispatch(setAuthUserData(res.userID, res.email, res.login, res.isAuth));
-			if (res.isAuth)
-				return 'setItem'
-		} else {
-			return res.errorMessage;
-		}
-	}
-
-	fakeLogin
-		.then(processingLoginResponse)
-		.then(res => {
-			expect(res).toBe('setItem')
-		})
-})
-
-it("Fake rember me shouldn't set value", () => {
-	const fakeLogin = new Promise(resolve => {
-		resolve({resultCode: 0, errorMessage: 'error',
-			login: 'akasha', email: "akasha@mail.ru", userID: 1, isAuth: false})
-	})
-
-	const processingLoginResponse = (res) => {
-		if (res.resultCode === 0) {
-			// dispatch(setAuthUserData(res.userID, res.email, res.login, res.isAuth));
-			if (res.isAuth)
-				return 'setItem'
-			else if (!res.isAuth)
-				return 'unsetItem'
-		} else {
-			return res.errorMessage;
-		}
-	}
-
-	fakeLogin
-		.then(processingLoginResponse)
-		.then(res => {
-			expect(res).toBe('unsetItem')
 		})
 })
