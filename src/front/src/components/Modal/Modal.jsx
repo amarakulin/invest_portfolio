2128a4490b2fcf0bb89090d8c3b11951fed9d1ff
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-25 17:43:24 +0300

    add portal to Modal component

--- src/front/src/components/Modal/Modal.jsx
+++ src/front/src/components/Modal/Modal.jsx
@@ -1,13 +1,15 @@
-import { ModalContainer, ModalDialog, CloseModalButton } from './ModalStyles'
+import { ModalContainer, ModalDialog, CloseModalButton } from './ModalStyles';
+import { createPortal } from 'react-dom';
 
 const Modal = (props) => {
-	return (
+	return createPortal(
 		<ModalContainer onClick={() => props.closeModal(false)} display={props.display ? 'block' : 'none'} >
 			<ModalDialog onClick={e => e.stopPropagation()} >
 				<CloseModalButton onClick={() => props.closeModal(false)} >&#10006;</CloseModalButton>
 				{props.children}
 			</ModalDialog>
-		</ModalContainer>
+		</ModalContainer>,
+		document.body
 	)
 }
 
