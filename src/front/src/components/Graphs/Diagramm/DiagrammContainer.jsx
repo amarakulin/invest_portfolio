256774d0027a2a222f0f2eabbeda870c72312dfe
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-25 15:28:26 +0300

    add diagram render function

--- src/front/src/components/Graphs/Diagramm/DiagrammContainer.jsx
+++ src/front/src/components/Graphs/Diagramm/DiagrammContainer.jsx
@@ -5,30 +5,39 @@ import { getDiagrammData, setActiveCircle } from '../../../redux/diagrammReduser
 import { GraphPreloaderContainer } from '../Graph/GraphUtils/GraphUtilsStyles'
 import Preloader from '../../Basic/Preloader/Preloader';
 
-const DiagrammContainer = (props) => {
-	useEffect(() => {
-		props.getDiagrammData();
-	}, [])
-
+const render = (props) => {
+	
 	const mouseEnterHandler = (i) => {
 		props.setActiveCircle(i);
 	}
-
+	
 	const mouseLeaveHandler = () => {
 		props.setActiveCircle(null);
 	}
 
-	return (
-		props.isFetching || !props.data
-		? <GraphPreloaderContainer> <Preloader color='black' /> </GraphPreloaderContainer>
-		: <Diagramm 
+	if (props.isFetching || !props.data) {
+		return <GraphPreloaderContainer> <Preloader color='black' /> </GraphPreloaderContainer>
+	} else if (props.data.length === 0) {
+		return 'Нет активов для отрисовки диаграммы'
+	} else {
+		return (
+			<Diagramm 
 				data={props.data}
 				setActiveCircle={props.setActiveCircle}
 				activeIndex={props.activeIndex}
 				mouseEnterHandler={mouseEnterHandler}
 				mouseLeaveHandler={mouseLeaveHandler}
 			/>
-	)
+		)
+	}
+}
+
+const DiagrammContainer = (props) => {
+	useEffect(() => {
+		props.getDiagrammData();
+	}, [])
+
+	return render(props)
 }
 
 const mapDispatchToProps = (state) => ({
