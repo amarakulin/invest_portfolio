d3068f4e2130ee97e63f789fab973bcd367e7cde
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-25 21:12:55 +0300

    compact new asset with modal to single component

---
+++ src/front/src/components/NewAsset/NewAsset.jsx
@@ -0,0 +1,18 @@
+import Modal from '../Modal/Modal';
+import AddNewAssetsForm from '../Forms/AddNewAssetsForm/AddNewAssetsForm';
+import Alert from '../Alert/Alert';
+import { connect } from 'react-redux';
+import { showAlert } from '../../redux/alertReduser';
+
+const NewAsset = (props) => {
+	return (
+		<>
+			<Modal closeModal={props.closeModal} display={props.display}>
+				<AddNewAssetsForm showAlert={props.showAlert} />
+			</Modal>
+			<Alert />
+		</>
+	)
+}
+
+export default connect(null, { showAlert })(NewAsset);
