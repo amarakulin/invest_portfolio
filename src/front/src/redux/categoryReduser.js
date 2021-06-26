4947538dbd85be020aee83734c934ebed98c51a6
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-27 00:58:01 +0300

    add getCategories thunk creator

--- src/front/src/redux/categoryReduser.js
+++ src/front/src/redux/categoryReduser.js
@@ -1,15 +1,41 @@
 import { CategoryApi } from '../api/api';
 
-const initialState = {
+const ADD_CATEGORY = 'ADD_CATEGORY';
+const SET_CATEGORIES = 'SET_CATEGORIES';
 
+const initialState = {
+	categories: []
 }
 
 const categoryResuser = (state = initialState, action) => {
 	switch (action.type) {
+		case ADD_CATEGORY: {
+			return {
+				...state,
+				categories: [...state.categories, action.category]
+			}
+		}
+		case SET_CATEGORIES: {
+			return {
+				...state,
+				categories: [...action.categories]
+			}
+		}
 		default: {
 			return state;
 		}
 	}
+}
+
+const addCategory = (category) => ({type: ADD_CATEGORY, category})
+
+const setCategories = (categories) => ({type: ADD_CATEGORY, categories})
+
+export const getCategories = () => (dispatch) => {//TODO добавить в init app
+	return CategoryApi.getCategories()
+		.then(res => {
+			dispatch(setCategories(res.categories))
+		})
 }
 
 export const createCategory = (data) => () => {
