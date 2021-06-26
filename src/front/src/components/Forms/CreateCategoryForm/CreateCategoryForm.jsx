ac8c942be79ab3f10ce3f3e72259a5c02d064221
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-06-26 22:44:25 +0300

    add Field wrapper

--- src/front/src/components/Forms/CreateCategoryForm/CreateCategoryForm.jsx
+++ src/front/src/components/Forms/CreateCategoryForm/CreateCategoryForm.jsx
@@ -7,6 +7,17 @@ import FormInput from '../../Basic/FormInput/FormInput';
 import { createCategory } from '../../../redux/categoryReduser';
 import { CreateCategoryFormSubmit } from '../../../utils/formSubmit';
 import { showAlert } from '../../../redux/alertReduser';
+import { StartWrapper } from '../../Basic/Wrapper/Wrapper';
+import { SmallTitle } from '../../Basic/Title/Title';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	display: grid;
+	gap: 20px 10px;
+	grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+	margin-bottom: 40px;
+`
 
 const CreateCategoryForm = (props) => {
 
@@ -18,19 +29,22 @@ const CreateCategoryForm = (props) => {
 			onSubmit={CreateCategoryFormSubmit(props.createCategory, props.showAlert)}
 			render={({ handleSubmit, submitting }) => (
 				<form onSubmit={handleSubmit}>
-					{
-						props.assetsData.map(el => {
-							return <Field
-								labelText={el.ticker}
-								name={el.ticker}
-								type='checkbox'
-								id={el.ticker}
-								key={el.ticker}
-							>
-								{props => <Checkbox {...props} />}
-							</Field>
-						})
-					}
+					<SmallTitle marginBottom={40}>Выберите активы для новой категории</SmallTitle>
+					<Wrapper>
+						{
+							props.assetsData.map(el => {
+								return <Field
+									labelText={el.ticker}
+									name={el.ticker}
+									type='checkbox'
+									id={el.ticker}
+									key={el.ticker}
+								>
+									{props => <Checkbox {...props} />}
+								</Field>
+							})
+						}
+					</Wrapper>
 					<Field
 						placeholder='Введите название новой категории'
 						labelText='Придумайте название новой категории'
