b16a2810b172b5308848ebb0be04f37589e89829
pichkasik <sasha.marakulin@mail.ru>
2021-06-29 11:12:43 +0300

    Documentation forms.sql

--- src/main/java/ru/akapich/invest_portfolio/model/forms/sql/FormDatePriceGraphSQLQuery.java
+++ src/main/java/ru/akapich/invest_portfolio/model/forms/sql/FormDatePriceGraphSQLQuery.java
@@ -5,12 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.akapich.invest_portfolio.model.portfolio.InvestPortfolio;
+import ru.akapich.invest_portfolio.repository.portfolio.history_data.HistoryAmountRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 /**
- * Form to get data from db for graph values
+ * Form to get data from db {@link HistoryAmountRepository} for graph values
  *
  * @author Aleksandr Marakulin
  **/
