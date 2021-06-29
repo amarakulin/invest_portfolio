84009f89530ed276159a448c3827b95a968a924a
pichkasik <sasha.marakulin@mail.ru>
2021-06-29 14:24:49 +0300

    Delete useless import

--- src/main/java/ru/akapich/invest_portfolio/validator/ValidatorController.java
+++ src/main/java/ru/akapich/invest_portfolio/validator/ValidatorController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
