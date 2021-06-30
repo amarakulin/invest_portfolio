d621ae72a933bf3275579e2798a06fc300bb58d2
pichkasik <sasha.marakulin@mail.ru>
2021-07-01 00:27:14 +0300

    Connect embedded database h2

--- src/test/java/ru/akapich/invest_portfolio/controller/user/UserControllerTest.java
+++ src/test/java/ru/akapich/invest_portfolio/controller/user/UserControllerTest.java
@@ -3,28 +3,26 @@ package ru.akapich.invest_portfolio.controller.user;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import ru.akapich.invest_portfolio.InvestPortfolioApplication;
 import ru.akapich.invest_portfolio.model.forms.login.RegistrationForm;
+import ru.akapich.invest_portfolio.repository.portfolio.InvestPortfolioRepository;
+import ru.akapich.invest_portfolio.repository.user.UserRepository;
 import ru.akapich.invest_portfolio.service.user.UserService;
+import ru.akapich.invest_portfolio.service.user.impl.UserDetailsServiceImpl;
 import ru.akapich.invest_portfolio.validator.login.ValidatorController;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -42,23 +40,49 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //@RunWith(SpringRunner.class)
 //@AutoConfigureTestDatabase
 //@WebMvcTest(UserController.class)
-@SpringBootTest
+//@PropertySource("classpath:application-test.properties")
+//@ActiveProfiles("test")
+
+//@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@SpringBootTest(classes = {
+		InvestPortfolioApplication.class,
+		UserController.class})
 @Import(ValidatorController.class)
-@PropertySource("classpath:application-test.properties")
+@ActiveProfiles("application.properties")
 public class UserControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
+	private UserRepository userRepository;
+
+
+	@Autowired
 	private UserController userController;
+
+	@Autowired
+	private InvestPortfolioRepository investPortfolioRepository;
+
+
+
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
 
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@MockBean
 	private BindingResult bindingResult;
+
+
+
+	@MockBean
+	private Model model ;
 
 	@Test
 	public void testUserControllerOnNull() throws Exception{
@@ -87,31 +111,19 @@ public class UserControllerTest {
 
 		this.mockMvc.perform(post("/api/auth/signup")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(toJson(registrationForm)))
+				.content(objectMapper.writeValueAsBytes(registrationForm)))
 				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+		this.mockMvc.perform(post("/api/auth/login")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.content("email=just_test@mail.com&password=just_test"))
+				.andDo(print())
+				.andExpect(status().isOk());
+
+
 	}
-
-//	@Test
-//	public void testGetToken() throws Exception {
-//		this.mockMvc.perform(get("/api/auth/token").with(user("lala_test").password("lala_test")))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-//	}
-
-//	@Test
-//	public void testRegistration() throws Exception {
-//		RegistrationForm registrationForm = new RegistrationForm("just_test", "just_test@mail.com", "just_test", "just_test");
-//
-//		this.mockMvc.perform(post("/api/auth/signup")
-//				.contentType(MediaType.APPLICATION_JSON)
-//				.content(toJson(registrationForm)))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-//	}
 
 	private String toJson(RegistrationForm registrationForm) throws JsonProcessingException {
 		ObjectMapper objectMapper = new ObjectMapper();
