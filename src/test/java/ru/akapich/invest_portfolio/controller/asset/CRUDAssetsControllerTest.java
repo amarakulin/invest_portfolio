62b1b4c256546583747db96a0e0ae6e2196e1611
pichkasik <sasha.marakulin@mail.ru>
2021-07-02 13:56:07 +0300

    Create test for NewAssets

--- src/test/java/ru/akapich/invest_portfolio/controller/asset/CRUDAssetsControllerTest.java
+++ src/test/java/ru/akapich/invest_portfolio/controller/asset/CRUDAssetsControllerTest.java
@@ -1,20 +1,16 @@
 package ru.akapich.invest_portfolio.controller.asset;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -22,14 +18,12 @@ import ru.akapich.invest_portfolio.InvestPortfolioApplication;
 import ru.akapich.invest_portfolio.controller.assets.CRUDAssetsController;
 import ru.akapich.invest_portfolio.model.forms.assets.NewAssetsForm;
 import ru.akapich.invest_portfolio.model.forms.login.RegistrationForm;
-import ru.akapich.invest_portfolio.model.portfolio.InvestPortfolio;
 import ru.akapich.invest_portfolio.model.portfolio.asset_data.store_assets.AllFinancialAsset;
-import ru.akapich.invest_portfolio.parser.info_assets.america.ParseInfoAmericanStock;
+import ru.akapich.invest_portfolio.model.portfolio.asset_data.store_assets.FinancialAssetInUse;
 import ru.akapich.invest_portfolio.repository.portfolio.asset_data.store_assets.AllFinancialAssetRepository;
-import ru.akapich.invest_portfolio.repository.portfolio.asset_data.store_assets.OwnedFinancialAssetRepository;
+import ru.akapich.invest_portfolio.repository.portfolio.asset_data.store_assets.FinancialAssetInUseRepository;
+import ru.akapich.invest_portfolio.repository.portfolio.history_data.HistoryAmountRepository;
 import ru.akapich.invest_portfolio.service.portfolio.asset_data.store_assets.AllFinancialAssetService;
-import ru.akapich.invest_portfolio.service.user.UserService;
-import ru.akapich.invest_portfolio.service.user.impl.UserDetailsServiceImpl;
 import ru.akapich.invest_portfolio.validator.login.ValidatorController;
 
 import javax.servlet.Filter;
@@ -38,15 +32,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.akapich.invest_portfolio.controller.user.UserControllerTest.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 /**
  * Test class for {@link CRUDAssetsController}
@@ -78,6 +69,9 @@ public class CRUDAssetsControllerTest {
 	private Filter springSecurityFilterChain;
 
 	@Autowired
+	private Environment env;
+
+	@Autowired
 	private WebApplicationContext context;
 
 	@BeforeEach
@@ -94,8 +88,6 @@ public class CRUDAssetsControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(registrationForm)))
 				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, "", SUCCESS_RESULT_CODE)))
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
 
 		this.mockMvc = MockMvcBuilders
@@ -116,14 +108,51 @@ public class CRUDAssetsControllerTest {
 				"currency", "USD",
 				"exchange", "NYSE",
 				"type", "Stock");
+		Map<String, String > map3 = Map.of("symbol", "A3",
+				"name", "name3",
+				"currency", "USD",
+				"exchange", "NYSE",
+				"type", "Stock");
+		Map<String, String > map4 = Map.of("symbol", "A4",
+				"name", "name4",
+				"currency", "USD",
+				"exchange", "NYSE",
+				"type", "Stock");
 		list.add(map1);
 		list.add(map2);
+		list.add(map3);
+		list.add(map4);
 		allFinancialAssetService.insertAllAssets(list);
 
 	}
 
 	@Test
-	public void testSetNewAssets() throws Exception{
+	public void testSuccessSeveralNewAssets() throws Exception{
+
+		List<NewAssetsForm> newAssetsFormList = new ArrayList<>();
+		List<AllFinancialAsset> allFinancialAssetList = allFinancialAssetRepository.findAll();
+		for(AllFinancialAsset asset : allFinancialAssetList){
+			if (!"A1".equals(asset.getTicker())) {
+				newAssetsFormList.add(
+						NewAssetsForm.builder()
+								.ticker(asset.getTicker())
+								.type(asset.getIdTypeAsset().getName())
+								.amount(BigDecimal.TEN)
+								.build());
+			}
+		}
+
+		this.mockMvc.perform(post("/api/data/newassets")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(newAssetsFormList)))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, "", SUCCESS_RESULT_CODE)))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+	}
+
+	@Test
+	public void testFailedNewAssetsNotExist() throws Exception{
 
 		List<NewAssetsForm> newAssetsFormList = new ArrayList<>();
 		AllFinancialAsset allFinancialAsset = allFinancialAssetRepository.findByTicker("A1");
@@ -133,51 +162,95 @@ public class CRUDAssetsControllerTest {
 				.amount(BigDecimal.TEN)
 				.build();
 		newAssetsFormList.add(newAssetsForm);
+		NewAssetsForm notExist = (NewAssetsForm) newAssetsForm.clone();
+		notExist.setTicker("NOTEXIST");
+		newAssetsFormList.add(notExist);
 
-
-//
-//		RegistrationForm registrationForm = RegistrationForm.builder()
-//				.email("user_1@mail.com")
-//				.name("user_1")
-//				.password("user_1")
-//				.rePassword("user_1")
-//				.build();
-//
-//		this.mockMvc.perform(post("/api/auth/signup")
-//				.contentType(MediaType.APPLICATION_JSON)
-//				.content(objectMapper.writeValueAsBytes(registrationForm)))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, "", SUCCESS_RESULT_CODE)))
-//				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-//
-//		this.mockMvc.perform(post("/api/auth/login").with(csrf())
-//				.contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-//				.content(String.format(TEMPLATE_LOGIN_FORM, registrationForm.getEmail(), registrationForm.getPassword())))
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(cookie().exists("JSESSIONID"))
-//				.andExpect(content().string(SUCCESS_RESPONSE_LOGIN));
-
-
-//		RequestBuilder requestBuilder = formLogin()
-//				.loginProcessingUrl("/api/auth/login")
-//				.userParameter("email")
-//				.passwordParam("password")
-//				.user("user_1@mail.com")
-//				.password("user_1");
-//		mockMvc.perform(requestBuilder)
-//				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(cookie().exists("JSESSIONID"));
-
+		String errorMessage = String.format("%s: %s", env.getProperty("valid.asset.not_exist"), notExist.getTicker());
 		this.mockMvc.perform(post("/api/data/newassets")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(newAssetsFormList)))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, "", SUCCESS_RESULT_CODE)))
+				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, errorMessage, ERROR_RESULT_CODE)))
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
-
 	}
+
+	@Test
+	public void testFailedNewAssetsNotInteger() throws Exception{
+
+		List<NewAssetsForm> newAssetsFormList = new ArrayList<>();
+		AllFinancialAsset allFinancialAsset1 = allFinancialAssetRepository.findByTicker("A1");
+		AllFinancialAsset allFinancialAsset2 = allFinancialAssetRepository.findByTicker("A2");
+		NewAssetsForm newAssetsForm1 = NewAssetsForm.builder()
+				.ticker(allFinancialAsset1.getTicker())
+				.type(allFinancialAsset1.getIdTypeAsset().getName())
+				.amount(BigDecimal.TEN)
+				.build();
+		NewAssetsForm newAssetsForm2 = NewAssetsForm.builder()
+				.ticker(allFinancialAsset2.getTicker())
+				.type(allFinancialAsset2.getIdTypeAsset().getName())
+				.amount(BigDecimal.TEN)
+				.build();
+
+		newAssetsForm2.setAmount(BigDecimal.valueOf(12.1));
+		newAssetsFormList.add(newAssetsForm1);
+		newAssetsFormList.add(newAssetsForm2);
+
+
+		String errorMessage = String.format("%s: %s", env.getProperty("valid.not_integer"), newAssetsForm2.getTicker());
+		this.mockMvc.perform(post("/api/data/newassets")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(newAssetsFormList)))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, errorMessage, ERROR_RESULT_CODE)))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+	}
+
+	@Test
+	public void testFailedNewAssetsRepeat() throws Exception{
+
+		List<NewAssetsForm> newAssetsFormList = new ArrayList<>();
+		AllFinancialAsset allFinancialAsset = allFinancialAssetRepository.findByTicker("A1");
+		NewAssetsForm newAssetsForm = NewAssetsForm.builder()
+				.ticker(allFinancialAsset.getTicker())
+				.type(allFinancialAsset.getIdTypeAsset().getName())
+				.amount(BigDecimal.TEN)
+				.build();
+		newAssetsFormList.add(newAssetsForm);
+		newAssetsFormList.add((NewAssetsForm) newAssetsForm.clone());
+
+		String errorMessage = env.getProperty("valid.assets.repeat");
+		this.mockMvc.perform(post("/api/data/newassets")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsBytes(newAssetsFormList)))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, errorMessage, ERROR_RESULT_CODE)))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+	}
+
+//	@Test
+//	public void testFailedNewAssetsInPortfolio() throws Exception{
+//
+//		List<NewAssetsForm> newAssetsFormList = new ArrayList<>();
+//		AllFinancialAsset allFinancialAsset = allFinancialAssetRepository.findByTicker("A1");
+//		NewAssetsForm newAssetsForm = NewAssetsForm.builder()
+//				.ticker(allFinancialAsset.getTicker())
+//				.type(allFinancialAsset.getIdTypeAsset().getName())
+//				.amount(BigDecimal.TEN)
+//				.build();
+//		newAssetsFormList.add(newAssetsForm);
+//
+//		String errorMessage = String.format("%s: %s", env.getProperty("valid.asset.in_portfolio"), newAssetsForm.getTicker());
+//
+//		this.mockMvc.perform(post("/api/data/newassets")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.content(objectMapper.writeValueAsBytes(newAssetsFormList)))
+//				.andDo(print())
+//				.andExpect(status().isOk())
+//				.andExpect(content().json(String.format(TEMPLATE_BASE_RESPONSE_FORM, errorMessage, ERROR_RESULT_CODE)))
+//				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//	}
 }
