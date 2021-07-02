ea64b812f7c3b12764692ad22c56c038b3d8ddc6
pichkasik <sasha.marakulin@mail.ru>
2021-07-02 13:55:56 +0300

    Add builder in NewAssetsFrom

--- src/main/java/ru/akapich/invest_portfolio/model/forms/assets/NewAssetsForm.java
+++ src/main/java/ru/akapich/invest_portfolio/model/forms/assets/NewAssetsForm.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+import ru.akapich.invest_portfolio.model.forms.login.RegistrationForm;
 
 import java.math.BigDecimal;
 
@@ -24,4 +25,37 @@ public class NewAssetsForm {
 	private String ticker;
 	private String type;
 	private BigDecimal amount;
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		NewAssetsForm newAssetsForm = null;
+		try{
+			newAssetsForm = (NewAssetsForm) super.clone();
+		}
+		catch (CloneNotSupportedException e){
+			newAssetsForm = NewAssetsForm.builder()
+					.amount(amount)
+					.type(type)
+					.ticker(ticker)
+					.build();
+		}
+		return newAssetsForm;
+	}
+
+//	Override
+//	public Object clone() throws CloneNotSupportedException {
+//		RegistrationForm registrationForm = null;
+//		try{
+//			registrationForm = (RegistrationForm) super.clone();
+//		}
+//		catch (CloneNotSupportedException e){
+//			registrationForm = RegistrationForm.builder()
+//					.name(this.name)
+//					.email(this.email)
+//					.password(this.password)
+//					.rePassword(this.rePassword)
+//					.build();
+//		}
+//		return registrationForm;
+//	}
 }
